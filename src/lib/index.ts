7c1f0f5210fec37d033561628a09bdb16127cb69
GregRos <life.gregr@gmail.com>
2024-08-06 17:21:21 +0300

    chore(build): fix cjs publish, add alternative export for bad IDEes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
-export { declare, type, type_of } from "./create-test/declare_test.js"
+export {
+    declare,
+    declare as declare_,
+    type,
+    type_of
+} from "./create-test/declare_test.js"
